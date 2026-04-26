97079e6e2d37003f7a73ca4481dd5ce4796e3e51
l33ton <deividadarov05@gmail.com>
2026-04-26 22:26:11 +0300

    trying to fix storage state

--- tests/web/test_user_profile.py
+++ tests/web/test_user_profile.py
@@ -2,8 +2,8 @@ import pytest
 from playwright.sync_api import expect
 from constants import *
 
-
-def test_if_navigate_to_my_profile_works(dashboard_page):
+@pytest.mark.skip_auth
+def test_if_navigate_to_my_profile_works(logged_in, dashboard_page):
     # Arrange
     # Our arrangement is the dashboard_page fixture
     # Act
@@ -16,8 +16,8 @@ def test_if_navigate_to_my_profile_works(dashboard_page):
     ("Ivan", "Georgiev"),
     ("VeryLongNameThatMightBreakTheUI", "Test"),
     ("O'Neil", "D-r")])
-
-def test_if_changing_name_works(dashboard_page, first_name, last_name, change_to_original_name):
+@pytest.mark.skip_auth
+def test_if_changing_name_works(logged_in, dashboard_page, first_name, last_name, change_to_original_name):
     # Arrange
     expected_result = f"Full name: {first_name} {last_name}"
     # Act
@@ -25,12 +25,12 @@ def test_if_changing_name_works(dashboard_page, first_name, last_name, change_to
     # Assert
     expect(dashboard_page.page.locator(FULL_NAME_LOCATOR)).to_contain_text(expected_result)
     # Cleanup - we are cleaning with a fixture called "change_to_original_name"
-
+@pytest.mark.skip_auth
 @pytest.mark.parametrize("new_password", [
     "JaKeTheDog1!",
     "Adventuretime1*",
     "FinnTheHuman2+"])
-def test_if_changing_password_works_with_valid_data(dashboard_page, new_password, change_password_to_common):
+def test_if_changing_password_works_with_valid_data(logged_in, dashboard_page, new_password, change_password_to_common):
     # Arrange
     # Our arrangement is mark.parametrize
     # Act
@@ -38,7 +38,7 @@ def test_if_changing_password_works_with_valid_data(dashboard_page, new_password
     # Assert
     expect(dashboard_page.page).to_have_url(EXPECTED_PROFILE_URL)
     # Cleanup - we are cleaning with a fixture called "change_password_to_common" and password is changed to the original one
-
+@pytest.mark.skip_auth
 @pytest.mark.parametrize("new_password", [
     "JaKeTheDog1",
     "Jake333",
