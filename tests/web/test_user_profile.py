f8322d29c088d8d2f5cf072dea2aec0a53a67e17
l33ton <deividadarov05@gmail.com>
2026-04-26 22:54:20 +0300

    added storage state

--- tests/web/test_user_profile.py
+++ tests/web/test_user_profile.py
@@ -2,7 +2,6 @@ import pytest
 from playwright.sync_api import expect
 from constants import *
 
-@pytest.mark.skip_auth
 def test_if_navigate_to_my_profile_works(logged_in, dashboard_page):
     # Arrange
     # Our arrangement is the dashboard_page fixture
@@ -16,7 +15,6 @@ def test_if_navigate_to_my_profile_works(logged_in, dashboard_page):
     ("Ivan", "Georgiev"),
     ("VeryLongNameThatMightBreakTheUI", "Test"),
     ("O'Neil", "D-r")])
-@pytest.mark.skip_auth
 def test_if_changing_name_works(logged_in, dashboard_page, first_name, last_name, change_to_original_name):
     # Arrange
     expected_result = f"Full name: {first_name} {last_name}"
@@ -25,7 +23,7 @@ def test_if_changing_name_works(logged_in, dashboard_page, first_name, last_name
     # Assert
     expect(dashboard_page.page.locator(FULL_NAME_LOCATOR)).to_contain_text(expected_result)
     # Cleanup - we are cleaning with a fixture called "change_to_original_name"
-@pytest.mark.skip_auth
+
 @pytest.mark.parametrize("new_password", [
     "JaKeTheDog1!",
     "Adventuretime1*",
@@ -38,7 +36,7 @@ def test_if_changing_password_works_with_valid_data(logged_in, dashboard_page, n
     # Assert
     expect(dashboard_page.page).to_have_url(EXPECTED_PROFILE_URL)
     # Cleanup - we are cleaning with a fixture called "change_password_to_common" and password is changed to the original one
-@pytest.mark.skip_auth
+
 @pytest.mark.parametrize("new_password", [
     "JaKeTheDog1",
     "Jake333",
