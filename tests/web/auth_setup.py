fa89bea2afff17f936a1ca05ac97f0124de3f947
l33ton <deividadarov05@gmail.com>
2026-04-27 15:40:08 +0300

    edited auth_setup with our environment

--- tests/web/auth_setup.py
+++ tests/web/auth_setup.py
@@ -1,8 +1,12 @@
+import os
 import pytest
-from constants import TEST_VALID_USERS, COMMON_PASSWORD
+from constants import TEST_VALID_USERS
+from dotenv import load_dotenv
 
 @pytest.mark.skip_auth
 def test_save_auth_state(login_page, context):
     customer = TEST_VALID_USERS["customer"]
-    login_page.login(customer["username"], COMMON_PASSWORD)
+    load_dotenv()
+    common_password = os.getenv("COMMON_PASSWORD")
+    login_page.login(customer["username"], common_password)
     context.storage_state(path="auth.json")
