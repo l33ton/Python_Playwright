e4f1c33aea9e3257653d718a5fb74f03292f950b
l33ton <deividadarov05@gmail.com>
2026-05-11 09:07:06 +0300

    configured auth_setup

--- tests/web/auth_setup.py
+++ tests/web/auth_setup.py
@@ -3,11 +3,17 @@ from playwright.sync_api import expect
 
 from constants import TEST_VALID_USERS, MY_DETAILS_LOCATOR
 from env_config import COMMON_PASSWORD
+from pages.login_page import LoginPage
+
 
 @pytest.mark.skip_auth
-def test_save_auth_state(login_page, context):
+def test_save_auth_state(page, login_page, context):
+    login_pg = LoginPage(page)
+
     employee = TEST_VALID_USERS["employee"]
-    login_page.login(employee["username"], COMMON_PASSWORD)
-    context.storage_state(path="auth.json")
+    login_pg.login(employee["username"], COMMON_PASSWORD)
 
     expect(login_page.page.locator(MY_DETAILS_LOCATOR)).to_be_visible()
+
+    context.storage_state(path="auth.json")
+    context.close()
